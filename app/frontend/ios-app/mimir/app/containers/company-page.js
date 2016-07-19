ad908c7423021fcb74dd291dfde08cef11e9c3be
Simon Lindgren <simon.g.lindgren@gmail.com>
2016-07-19 12:14:45 +0200

    connected company page container to the stores state and bound action: select_tab

--- app/frontend/ios-app/mimir/app/containers/company-page.js
+++ app/frontend/ios-app/mimir/app/containers/company-page.js
@@ -1,8 +1,12 @@
 'use strict';
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TabBarIOS } from 'react-native';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default class CompanyPage extends Component {
+import { select_tab } from '../actions/navigation.actions';
+
+class CompanyPage extends Component {
   render() {
     console.log(this.props);
     return (
@@ -12,6 +16,21 @@ export default class CompanyPage extends Component {
     );
   }
 }
+
+export default connect(
+  (state) => ({
+    state: {
+      user: state.user,
+      stocks: state.stocks,
+      navigation: state.navigation
+    }
+  }),
+  (dispatch) => ({
+    actions: bindActionCreators({
+      select_tab
+    }, dispatch)
+  })
+)(CompanyPage);
 
 const styles = StyleSheet.create({
   container: {
