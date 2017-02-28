cdfd19999fc94662da23bcb65dd508edbe4e80c6
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-02-28 18:05:14 +0100

    Added host argument to admin page pg connection, created rethinkdb proxy to get into docker bridge network from outside world

--- backend/admin-page/server/postgres.go
+++ backend/admin-page/server/postgres.go
@@ -7,7 +7,7 @@ import (
 )
 
 func connectPostgres(config pgConfig) *sql.DB {
-  connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", config.user, config.pwd, config.db)
+  connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", config.host, config.user, config.pwd, config.db)
   db, err := sql.Open("postgres", connStr)
   checkErr(err)
   err = db.Ping()
