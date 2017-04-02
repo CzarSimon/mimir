a90bb68651826e172f77b9f9eaf69127d205cbaa
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-04-02 15:40:35 +0200

    Removed old app folder

--- app/frontend/ios-app/mimir/index.ios.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
-import App from './app/containers/app';
-
-AppRegistry.registerComponent('mimir', () => App);
