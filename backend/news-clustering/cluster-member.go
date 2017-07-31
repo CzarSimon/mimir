db25351789d85357344c8e2d955bf7c8be6ff45d
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-07-31 16:09:16 +0200

    Update cluster-member.go

--- backend/news-clustering/cluster-member.go
+++ backend/news-clustering/cluster-member.go
@@ -1,9 +1,14 @@
 package main
 
-// ClusterMember represent an article in a cluester and it score
+// ClusterMember represent an article in a cluester and its score
 type ClusterMember struct {
 	ClusterHash 	string
 	UrlHash     	string
 	ReferenceScore 	float64
 	SubjectScore 	float64
+}
+
+// CalcCompoundScore Sums an articles Reference and Subject scorea and returns the result
+func CalcCompoundScore(member ClusterMember) float64 {
+	return member.ReferenceScore + member.SubjectScore
 }
