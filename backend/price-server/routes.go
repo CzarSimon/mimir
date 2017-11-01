8fa62d69cefc669f10c80be94cdd264fcfec75c8
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-11-01 14:46:17 +0100

    price-server: Added get latest prices route

--- backend/price-server/routes.go
+++ backend/price-server/routes.go
@@ -5,6 +5,7 @@ import "net/http"
 // SetupRoutes Sets up routes and route handlers
 func SetupRoutes(env *Env) *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/api/price", env.GetHistoricalPrices)
+	mux.HandleFunc("/api/price/historical", env.GetHistoricalPrices)
+	mux.HandleFunc("/api/price/latest", env.GetLatestPrices)
 	return mux
 }
