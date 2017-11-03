8e8e6f74c2ffd584eca273ea18d7e8572cf7d161
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-11-03 16:40:43 +0100

    lib/queue-client: Added serialization for QueueItem

--- lib/queue-client/queue-item.go
+++ lib/queue-client/queue-item.go
@@ -1,6 +1,8 @@
 package qc
 
 import (
+	"encoding/json"
+	
 	"github.com/CzarSimon/mimir/lib/news"
 )
 
@@ -9,4 +11,29 @@ type QueueItem struct {
 	URL      string         `json:"url"`
 	Depth    int16          `json:"depth"`
 	Subjects []news.Subject `json:"subjects"`
+}
+
+// GetKey Creates key for a QueueItem
+func (item QueueItem) GetKey() string {
+	return news.CreateURLHash(item.URL)
+}
+
+// Serialize Creates a json marshalled string of bytes for a QueueItem
+func (item QueueItem) Serialize() (string, error) {
+	bytes, err := json.Marshal(&item)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+// UnserializeQueueItem Creates a QueueItem from a serialized string
+func UnserializeQueueItem(byteStr string) (QueueItem, error) {
+	var item QueueItem
+	bytes := []bytes(byteStr)
+	err = json.Unmarshall(bytes, &item)
+	if err != nil {
+		return item, err
+	}
+	return item, nil
 }
