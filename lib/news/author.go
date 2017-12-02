8cfeda6e01bda11be62bef742b2d2c66c9de17fb
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-12-03 00:46:33 +0100

    lib/news: Added author and rank object

---
+++ lib/news/author.go
@@ -0,0 +1,7 @@
+package news
+
+//Author contains info about an article refererer.
+type Author struct {
+	ID            int64 `json:"id"`
+	FollowerCount int64 `json:"followerCount"`
+}
---
+++ lib/news/rankObject.go
@@ -0,0 +1,9 @@
+package news
+
+//RankObject contains info to scrape and rank an article
+type RankObject struct {
+	Urls     []string  `json:"urls"`
+	Subjects []Subject `json:"subjects"`
+	Author   Author    `json:"author"`
+	Language string    `json:"language"`
+}
