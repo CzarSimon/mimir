021d4c78c4bed73f89483ec2aae8e1582c954560
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-12-04 11:15:54 +0100

    Update ranker_client.go

--- backend/tweet-handler/ranker_client.go
+++ backend/tweet-handler/ranker_client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/CzarSimon/mimir/lib/news"
 )
@@ -10,4 +11,15 @@ import (
 func (env *Env) SendLinksToRanker(tweet Tweet, subjects []news.Subject) {
 	log.Println(env.Config.ranker.ToURL("api/rank-article"))
 	log.Println(tweet.Text)
+}
+
+// twitterUserToAuthor Converts a TwitterUser to Author type
+func twitterUserToAuthor(user TwitterUser) (news.Author, error) {
+	var author news.Author
+	author.ID, err := strconv.Atoi(user.ID)
+	if err != nil {
+		return author, err
+	}
+	author.FollowerCount, err = strconv.Atoi(user.Followers)
+	return author, err
 }
