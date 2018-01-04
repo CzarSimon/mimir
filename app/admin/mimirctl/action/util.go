a997ffbe9a0b51cd6f05359297d667824d89e977
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-01-04 10:59:38 +0100

    mimirctl/action added hidden user input

--- app/admin/mimirctl/action/util.go
+++ app/admin/mimirctl/action/util.go
@@ -3,23 +3,21 @@ package action
 import (
 	"fmt"
 	"log"
-
+	
+	"golang.org/x/crypto/ssh/terminal"
 	"github.com/CzarSimon/mimir/app/admin/mimirctl/api"
 	"github.com/urfave/cli"
 )
 
 // Placeholder Action to user for command development
 func Placeholder(c *cli.Context) error {
-	fmt.Printf("%+v\n", c.Args)
+	fmt.Printf("Command: mimirctl %s", c.Args(1))
 	return nil
 }
 
 // testApiConfig Checks if a supplied config is correct
 func testApiConfig(config api.Config) {
-	if err := api.Ping(config); err != nil {
-		fmt.Println(err.Error())
-		log.Fatal()
-	}
+	checkErr(api.Ping(config))
 	fmt.Println("Success! mimirctl authenticated")
 }
 
@@ -38,4 +36,20 @@ func getInputWithDefault(key, defaultVal string) string {
 		return defaultVal
 	}
 	return value
+}
+
+// getHiddenInput Gets hidden user input from stdin
+func getHiddenInput(key string) string {
+	fmt.Printf("%s: \n", key)
+	value, err := terminal.ReadPassword(0)
+	checkErr(err)
+	return string(value)
+}
+
+// checkErr Prints error and halts execution if error is not nil
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		log.Fatal()
+	}
 }
