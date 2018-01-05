b2d921db317b50db6d2c05da87b92cbfe503f751
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-01-05 16:39:07 +0100

    lib/stock Added string represntaiton in go package

--- app/lib/go/schema/stock/stock.go
+++ app/lib/go/schema/stock/stock.go
@@ -1,5 +1,9 @@
 package stock
 
+import (
+  "fmt"
+)
+
 // Stock Contains information about a stock and issuing company
 type Stock struct {
   Ticker      string   `json:"ticker"`
@@ -8,4 +12,11 @@ type Stock struct {
   ImageURL    string   `json:"imageUrl,omitempty"`
   Website     string   `json:"website,omitempty"`
   Keywords    []string `json:"keyworks,omitempty"`
+}
+
+// String Returns a string representation of a stock
+func (stock Stock) String() string {
+  return fmt.Sprintf(
+    "Ticker=%s Name=%s\nDescription=%s\nImageURL=%s\nWebsite=%s",
+    stock.Ticker, stock.Name, stock.Description, stock.ImageURL, stock.Website)
 }
