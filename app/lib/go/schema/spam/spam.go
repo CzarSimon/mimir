139280ab414d468fbace32685d2bdfc0b529d056
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-01-05 16:31:53 +0100

    Create spam.go

---
+++ app/lib/go/schema/spam/spam.go
@@ -0,0 +1,34 @@
+package spam
+
+import (
+  "fmt"
+)
+
+// Supported spam labels
+const (
+  SPAM_LABEL     = "SPAM"
+  NON_SPAM_LABEL = "NON-SPAM"
+)
+
+// Candidate Struct that can be classified as spam
+type Candidate struct {
+  Text  string `json:"text"`
+  Label string `json:"label"`
+}
+
+// NewCandidate Creates a new unlabeled spam Candidate
+func NewCandidate(text string) Candidate {
+  return Candidate{
+    Text: text,
+  }
+}
+
+// IsSpam Checks whether a spam candidate is classified as spam
+func (candidate Candidate) IsSpam() bool {
+  return candidate == SPAM_LABEL
+}
+
+// String Returns a string representation of a spam Candidate
+func (candidate Candidate) String() string {
+  return fmt.Sprintf("Text=%s Label=%s", candidate.Text, candidate.Label)
+}
