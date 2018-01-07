38ace3b592c738d421dc528e413e02a86bc96653
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-01-07 12:43:39 +0100

    added get stocks

--- app/admin/admin-api/stock_handler.go
+++ app/admin/admin-api/stock_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
@@ -9,7 +10,7 @@ import (
 	"github.com/CzarSimon/util"
 )
 
-// stockHandler Handles request related to the stock resource
+// stockHandler handles request related to the stock resource.
 func (env *Env) stockHandler(w http.ResponseWriter, r *http.Request) error {
 	switch r.Method {
 	case http.MethodGet:
@@ -22,7 +23,49 @@ func (env *Env) stockHandler(w http.ResponseWriter, r *http.Request) error {
 	}
 }
 
-// storeNewStock Derializes and stores a new stock
+// getStocks querys for and sends tracked stocks to the requestor.
+func (env *Env) getStocks(w http.ResponseWriter, r *http.Request) error {
+	stocks, err := queryForStocks(r, env.AppDB)
+	if err != nil {
+		log.Println(err)
+		return httputil.InternalServerError
+	}
+	return httputil.SendJSON(w, stocks)
+}
+
+// queryForStocks querys the database for tracked stocks
+// based on the request query
+func queryForStocks(r *http.Request, db *sql.DB) ([]stock.Stock, error) {
+	rows, err := db.Query(getStockQuery())
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	return constructStocksList(rows)
+}
+
+// constructStocksList creates a list of stocks from a result set
+func constructStocksList(rows *sql.Rows) ([]stock.Stock, error) {
+	stocks := make([]stock.Stock, 0)
+	var s stock.Stock
+	for rows.Next() {
+		err := rows.Scan(&s.Ticker, &s.Ticker, &s.Description, &s.ImageURL, &s.Website)
+		if err != nil {
+			return nil, err
+		}
+		stocks = append(stocks, s)
+	}
+	return stocks, nil
+}
+
+// getStockQuery creates query for tracked stocks.
+func getStockQuery() string {
+	return `
+		SELECT TICKER, NAME, DESCRIPTION, IMAGE_URL, WEBSITE
+		FROM STOCK`
+}
+
+// storeNewStock stores a new stock.
 func (env *Env) storeNewStock(w http.ResponseWriter, r *http.Request) error {
 	var newStock stock.Stock
 	err := util.DecodeJSON(r.Body, &newStock)
@@ -30,7 +73,7 @@ func (env *Env) storeNewStock(w http.ResponseWriter, r *http.Request) error {
 		log.Println(err)
 		return httputil.BadRequest
 	}
-	log.Printf("Ticker=%s Name=%s", newStock.Ticker, newStock.Name)
+	log.Println(newStock)
 	httputil.SendOK(w)
 	return nil
 }
