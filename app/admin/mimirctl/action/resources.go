1c8168f86003c6b533803d7092be9c1a8f4e7007
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-01-09 08:47:53 +0100

    mimirctl: Swithced back to using resource type

--- app/admin/mimirctl/action/resources.go
+++ app/admin/mimirctl/action/resources.go
@@ -18,10 +18,10 @@ const (
 type ResourceType string
 
 // ResourceMap maps a resource to an action function
-type ResourceMap map[string]cli.ActionFunc
+type ResourceMap map[ResourceType]cli.ActionFunc
 
 // GetFunc gets an action function based on a resource type. Exits if unsuccessful
-func (rm ResourceMap) GetFunc(resource string) cli.ActionFunc {
+func (rm ResourceMap) GetFunc(resource ResourceType) cli.ActionFunc {
 	function, ok := rm[resource]
 	if !ok {
 		checkErr(fmt.Errorf("No action exists for resource \"%s\"", resource))
@@ -30,7 +30,7 @@ func (rm ResourceMap) GetFunc(resource string) cli.ActionFunc {
 }
 
 // getResource gets a resource type from supplied command line arguments
-func getResource(c *cli.Context) string {
+func getResource(c *cli.Context) ResourceType {
 	resourceArgIndex := 0
-	return c.Args().Get(resourceArgIndex)
+	return ResourceType(c.Args().Get(resourceArgIndex))
 }
