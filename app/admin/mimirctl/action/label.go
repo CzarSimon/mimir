45f68a04be4ff02bc5c490c5cb94954e06778c8d
Simon Lindgren <simon.g.lindgren@gmail.com>
2018-01-10 19:22:51 +0100

    mimirctl: changed labelSpam

--- app/admin/mimirctl/action/label.go
+++ app/admin/mimirctl/action/label.go
@@ -1,8 +1,10 @@
 package action
 
 import (
+	"bytes"
 	"fmt"
 
+	"github.com/CzarSimon/mimir/app/admin/mimirctl/api"
 	"github.com/CzarSimon/mimir/app/lib/go/schema/spam"
 	"github.com/urfave/cli"
 )
@@ -28,17 +30,33 @@ func getSpamCandidates(c *cli.Context) error {
 	return nil
 }
 
+// labelSpam interacts with the user to label a spam candidate.
 func labelSpam(candidate spam.Candidate) {
 	fmt.Printf("Text: %s \nChose label (press any other key to skip)\n")
-	choice := getInput(fmt.Sprintf(
-		"1. - %s , 0. - %s", spam.SPAM_LABEL, spam.NON_SPAM_LABEL))
-	if choice == "1" {
-		candidate.Label = spam.SPAM_LABEL
-	} else if choice == "2" {
-		candidate.Label = spam.NON_SPAM_LABEL
-	} else {
+	choice := getInput(spamChoices.String())
+	label, ok := spamChoices[choice]
+	if !ok {
 		fmt.Println("Skipping")
 		return
 	}
+	candidate.Label = label
 	fmt.Println(candidate)
+}
+
+// choiceMap map used for linking user input to a string label.
+type choiceMap map[string]string
+
+// spamChoices map for linking choices to spam labels.
+var spamChoices = choiceMap{
+	"1": spam.SPAM_LABEL,
+	"0": spam.NON_SPAM_LABEL,
+}
+
+// String returns a string representation of a choiceMap.
+func (cm choiceMap) String() string {
+	var buf bytes.Buffer
+	for key, choice := range cm {
+		buf.WriteString(fmt.Sprintf("%s. - %s\n", key, choice))
+	}
+	return buf.String()
 }
--- app/admin/mimirctl/api/get.go
+++ app/admin/mimirctl/api/get.go
@@ -23,18 +23,22 @@ func GetUntrackedTickers() []string {
 	return tickers
 }
 
+// GetSpamCandidates gets a list of spam candidates from the admin api.
 func GetSpamCandidates() []spam.Candidate {
 	candidates := make([]spam.Candidate, 0)
 	getJSON("spam", &candidates)
 	return candidates
 }
 
+// getJSON makes a get request returning a json body which
+// is decoded into a supplid container.
 func getJSON(resource string, v interface{}) {
 	resp, err := makeGetRequest(createRoute(resource))
 	checkErr(err)
 	err = util.DecodeJSON(resp.Body, v)
 }
 
+// checkErr inspectes an error, logs it and exits if not nil.
 func checkErr(err error) {
 	if err != nil {
 		fmt.Println(err)
